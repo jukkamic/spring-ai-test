53cb8f56bdcd1495bde158a4fe36b5543001ae1b
Jukka Michelsson <jukkamic@gmail.com>
2026-03-14 22:05:01 +0200

    Add Category. Continued from prev commmit

---
+++ src/main/java/fi/kotkis/springai/domain/Category.java
@@ -0,0 +1,54 @@
+package fi.kotkis.springai.domain;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Material> materials = new HashSet<>();
+
+    // Default constructor required by JPA
+    public Category() {
+    }
+
+    // Constructor with fields
+    public Category(String name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Material> getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(Set<Material> materials) {
+        this.materials = materials;
+    }
+}
